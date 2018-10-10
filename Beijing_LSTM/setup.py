e37ed6b7c444b5f25780e11d857b5164b3405bb1
Peter_Jiang <865268033@qq.com>
2018-10-10 20:46:42 +0800

    data_proccessing

---
+++ Beijing_LSTM/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='Beijing_LSTM',
+    version='1.0',
+    packages=[''],
+    url='http://',
+    license='',
+    author='Peter_Bonnie',
+    author_email='',
+    description=''
+)
