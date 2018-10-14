3deef2cd61b68fe0fe6cbca705d2e80930fd86de
Peter_Jiang <865268033@qq.com>
2018-10-14 19:29:26 +0800

    data_proccessing

--- Beijing_LSTM/SVM.py
+++ Beijing_LSTM/SVM.py
@@ -8,7 +8,7 @@ import  numpy as np
 import pandas as pd
 from sklearn.svm import libsvm
 from sklearn.preprocessing import MinMaxScaler,StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 from sklearn.datasets import load_digits
 from math import fabs
 from sklearn.metrics import accuracy_score,auc,recall_score,f1_score,precision_score,roc_curve,roc_auc_score,confusion_matrix
@@ -16,7 +16,6 @@ from sklearn import  metrics
 import tensorflow as tf
 import  matplotlib.pyplot as plt
 import csv
-
 """
 主要是使用的SVM实现多分类，主要有两种方法：1.一对一方法,当K类时，需要创建k(k-1)/2个分类器 
                                     2.还有一种一对多方法，为每个类别创建一个高斯核函数分类器，最后的预测类别是具有最大SVM间隔的类别
@@ -80,10 +79,7 @@ def crate_data_to_target(train_X,train_Y,test_X,test_Y):
     test_data_Y3=np.array([1 if y==2 else -1 for y in test_Y])
     test_data_Y4=np.array([1 if y==3 else -1 for y in test_Y])
     test_data_Y=np.array([test_data_Y1,test_data_Y2,test_data_Y3,test_data_Y4])
-    # class_0_x=[x for i,x in enumerate(train_data_X) if train_Y[i]==0]
-    # class_1_x=[x for i,x in enumerate(train_data_X) if train_Y[i]==1]
-    # class_2_x=[x for i,x in enumerate(train_data_X) if train_Y[i]==2]
-    # class_3_x=[x for i,x in enumerate(train_data_X) if train_Y[i]==3]
+
     return train_data_X,train_data_Y,test_data_X,test_data_Y
 
 def open_csv(test_value,test_label):
@@ -105,11 +101,17 @@ def reshape_mat(mat,batch_size):
     v2=tf.reshape(v1,[4,batch_size,1])
     return (tf.matmul(v2,v1))
 if __name__=="__main__":
-    data_X,data_Y=open_csv("test_value.csv","test_label.csv")
-    train_X,test_X,train_Y,test_Y=train_test_split(data_X,data_Y,test_size=0.7,random_state=20)
+    # data_X,data_Y=open_csv("test_value.csv","test_label.csv")
+    data=pd.read_csv("error_dataset.csv")
+    data_X=data['value'].values
+    data_Y=data['class'].values
+    train_X,test_X,train_Y,test_Y=train_test_split(data_X,data_Y,test_size=0.4,random_state=42)
     train_data_X,train_data_Y,test_data_X,test_data_Y=crate_data_to_target(train_X,train_Y,test_X,test_Y)
+
+    np.savetxt("svm_test_x.txt",test_data_X)
+    np.savetxt("svm_test_y.txt",test_data_Y)
     #定义一些常量和占位符
-    batch_size = 20
+    batch_size = 100
     X_data=tf.placeholder(shape=[None,1],dtype=tf.float32)
     Y_target=tf.placeholder(shape=[4,None],dtype=tf.float32)
     prediction_grid=tf.placeholder(shape=[None,1],dtype=tf.float32)
@@ -142,14 +144,22 @@ if __name__=="__main__":
     accuracy=tf.reduce_mean(tf.cast(tf.equal(prediction,tf.argmax(Y_target,0)),tf.float32))
 
     #初始化相关变量
-    opt=tf.train.GradientDescentOptimizer(0.01)
+    opt=tf.train.GradientDescentOptimizer(0.001)
     train_step=opt.minimize(loss)
 
+    saver=tf.train.Saver()
     init=tf.global_variables_initializer()
     sess=tf.Session()
     sess.run(init)
     loss_vec=[]
     batch_accuracy=[]
+    max_score={
+        'f1_score':0.0,
+        'accuracy':0.0,
+        'precision':0.0,
+        'Recall':0.0,
+        'index':0
+    }
     for i in range(1000):
         rand_index=np.random.choice(len(train_data_X),size=batch_size)
         rand_x=train_data_X[rand_index].reshape([batch_size,-1])
@@ -166,48 +176,29 @@ if __name__=="__main__":
                     if rand_y[j][p]==1:
                         true_label.append(j)
                         continue
+            true_label=np.array(list(true_label))
+            pred_output=np.array(list(pred_output))
             print("true_value:",true_label)
             print("pred_value:",pred_output)
-        print("epoch:{}=======loss:{:.9f}========accuracy:{:.9f}".format(str(i+1),temp_loss,temp_accuracy))
+            print("f1_score:", f1_score(true_label, pred_output, average='macro'))
+            print("accuracy:", accuracy_score(true_label, pred_output))
+            print("precision:", precision_score(true_label, pred_output, average='macro'))
+            print("Recall:", recall_score(true_label, pred_output, average='macro'))
+            fpr, tpr,thresholds = roc_curve(true_label,pred_output, pos_label=1)
 
-    #metrics
-    test_data_X=np.reshape(test_data_X,[-1,1])
-    # print(test_data_X.shape)
-    # print(test_data_Y.shape)  #这里报错的原因是形状不匹配，所以我需要重新定义形状
+            #获取最好的分数
+            if max_score['f1_score']<f1_score(true_label, pred_output, average='macro'):
+                max_score['f1_score']=f1_score(true_label, pred_output, average='macro')
+                max_score['accuracy']=accuracy_score(true_label, pred_output)
+                max_score['precision']=precision_score(true_label, pred_output, average='macro')
+                max_score['Recall']=recall_score(true_label, pred_output, average='macro')
+                max_score['index']=i-1
+            print("auc:", auc(fpr,tpr))
+            print("epoch:{}=======loss:{:.9f}========accuracy:{:.9f}".format(str(i+1),temp_loss,temp_accuracy))
+    print(max_score)
+    save_path=saver.save(sess,"SVM_Model/SVM.ckpt")
+    print("model has been stored in {}".format(save_path))
 
-    #利用测试集数据来评估模型分类的好坏
-    for i in  range(30):
-        print("================epoch:{}==========================".format(str(i+1)))
-        rand_test_index=np.random.choice(len(test_data_X),size=batch_size,replace=False)
-        rand_test_x=test_data_X[rand_test_index]
-        rand_test_y=test_data_Y[:,rand_test_index]
-        val_accuracy,y_pred=sess.run([accuracy,prediction],feed_dict={X_data:rand_test_x,Y_target:rand_test_y,prediction_grid:rand_test_x})
-        print("validation accuracy:",val_accuracy)
-        y_true=[]
-        for p in range(rand_y.shape[1]):
-            for j in range(rand_y.shape[0]):
-                if rand_y[j][p] == 1:
-                    y_true.append(j)
-                    continue
-        y_true=np.array(list(y_true))
-        y_pred=np.array(list(y_pred))
-        # y_true=np.argmax(y_true,1)
-        print("accuracy:",accuracy_score(y_true,y_pred))
-        print("precision:",precision_score(y_true,y_pred,average='macro'))
-        print("Recall:",recall_score(y_true,y_pred,average='macro'))
-        print("f1_score:",f1_score(y_true,y_pred,average='macro'))
-
-        # print("confusion matrix:",confusion_matrix(y_true,y_pred))
-        # fpr,tpr,thresholds=roc_curve(y_true,y_pred)
-        # print("tpr:{},fpr:{},thresholds:{}".format(fpr,tpr,thresholds))
-
-    plt.figure()
-    plt.plot(batch_accuracy, 'r--', label='Accuracy',lw=3)
-    plt.title('Batch Accuracy')
-    plt.xlabel('Generation')
-    plt.ylabel('Accuracy')
-    plt.legend(loc='lower right')
-    plt.show()
 
 
 
